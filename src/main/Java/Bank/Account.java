45e10b37d2799dbbb0feeaf893d4d706a2c5d215
ShevtsovDmitriy <shevtsovdiman@gmail.com>
2015-03-07 20:18:30 +0300

    DBAPI can return DBObject, Dao can cast them to User(need to make it universal )(2)

--- src/main/Java/Bank/Account.java
+++ src/main/Java/Bank/Account.java
@@ -1,11 +1,17 @@
 package Bank;
 
+import java.sql.Date;
+
 /**
  * Created by Дмитрий on 05.03.2015.
  */
 public class Account {
 
     private double amount;
+    private Date startDate;
 
-
+    public Account(double amount, Date startDate) {
+        this.amount = amount;
+        this.startDate = startDate;
+    }
 }
--- src/main/Java/DBConnection/ResultSetHandler.java
+++ src/main/Java/DBConnection/ResultSetHandler.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 
 /**
  * Created by Дмитрий on 06.02.2015.
+ * Интерфейс для ResultSetHandler`а, который преобразует ResultSet в удобоваримое представление
  */
 public interface ResultSetHandler<T> {
 
